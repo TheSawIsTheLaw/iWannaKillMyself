867f168507d3ab28ebce8be8f53825a978259721
TheSawIsTheLaw <dimakrok@bk.ru>
2024-02-25 20:53:17 +0300

    ADD: IT WORKS, IT FUCKING WORKS AHAHAHAHAHAHAHAHAHHAH

---
+++ dataset/dirt_tongue/dirt_tongue.py
@@ -0,0 +1,29 @@
+import re
+
+standart_dirt = 'хуй|бля|еб|пизд|ёб|хуё'
+
+def _get_search(pattern: str):
+    """
+    return function to search words with the pattern
+    """
+    def hide_search(word: str) -> bool:
+        return bool(re.search(pattern, word))
+
+    return hide_search
+
+
+def is_dirt(pattern: str=standart_dirt):
+    """
+    return function to search pattern in text
+    """
+    funk = _get_search(pattern)
+
+    def hide_search(text: str) -> bool:
+        for word in re.findall(r'\w+', text):
+            if funk(word.lower()):
+                return True
+        
+        return False
+
+    return hide_search
+
